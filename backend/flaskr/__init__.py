4f19055aa475d7650523f9e83314942a9c4fd4b2
yousifelhady <yousif.abdelhamed@gmail.com>
2021-01-06 00:03:11 +0200

    setting CORS and after_request decorator

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,6 +8,14 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, questions):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+    formated_questions = [question.format() for question in questions]
+    current_questions = formated_questions[start:end]
+    return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
@@ -16,10 +24,16 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
+  CORS(app)
 
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE')
+        return response
 
   '''
   @TODO: 
