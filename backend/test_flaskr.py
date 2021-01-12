a860161f2b6a5beffb98339ad250545ea98eed4b
yousifelhady <yousif.abdelhamed@gmail.com>
2021-01-12 19:26:43 +0200

    implementing unit test for some end points (get_category, get_questions, add_question, search_questions)

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,8 +15,15 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format('Yousif', 'yousif', 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            'question': 'What is the formula of Hydrochloric Acid?',
+            'answer': 'HCL',
+            'category': '1',
+            'difficulty': 3
+        }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -33,7 +40,61 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        res_data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data['success'], True)
+        self.assertTrue(res_data['categories'])
+
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        res_data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data['success'], True)
+        self.assertTrue(res_data['questions'])
+        self.assertTrue(res_data['total_questions'])
+        self.assertTrue(res_data['categories'])
+
+    #def test_delete_questions(self):
+    #    question_id = 10
+    #    res = self.client().delete('/questions/{}'.format(question_id))
+    #    res_data = json.loads(res.data)
+
+    #    self.assertEqual(res.status_code, 200)
+    #    self.assertEqual(res_data['success'], True)
+    #    self.assertEqual(res_data['question_id'], question_id)
+
+    def test_add_question(self):
+        res = self.client().post('/questions', json=self.new_question)
+        res_data = json.loads(res.data)
+        added_question = Question.query.order_by(Question.id.desc()).first()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data['success'], True)
+        self.assertEqual(res_data['question_id'], added_question.id)
+
+    def test_search_question(self):
+        search_term = 'title'
+        res = self.client().get('/questions/search', query_string={'searchTerm': search_term})
+        res_data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data['success'], True)
+        self.assertTrue(res_data['questions'])
+        self.assertTrue(res_data['total_questions'], len(res_data['questions']))
+
+    def test_search_question_no_match(self):
+        search_term = 'blabla'
+        res = self.client().get('/questions/search', query_string={'searchTerm': search_term})
+        res_data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_data['success'], True)
+        self.assertEqual(res_data['questions'], [])
+        self.assertEqual(res_data['total_questions'], 0)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
